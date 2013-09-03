fe5723e936bc691271f7629b75d184a8150b4634
Damian Gryski <damian@gryski.com>
2013-09-03 22:36:41 +0200

    test more more EOF conditions

--- bitstream_test.go
+++ bitstream_test.go
@@ -7,6 +7,58 @@ import (
 	"testing"
 )
 
+func TestBitStreamEOF(t *testing.T) {
+
+	br := NewReader(strings.NewReader("0"))
+
+	b, err := br.ReadByte()
+	if b != '0' {
+		t.Error("ReadByte didn't return first byte")
+	}
+
+	b, err = br.ReadByte()
+	if err != io.EOF {
+		t.Error("ReadByte on empty string didn't return EOF")
+	}
+
+	// 0 = 0b00110000
+	br = NewReader(strings.NewReader("0"))
+
+	buf := bytes.NewBuffer(nil)
+	bw := NewWriter(buf)
+
+	for i := 0; i < 4; i++ {
+		bit, err := br.ReadBit()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			t.Error("GetBit returned error err=", err.Error())
+			return
+		}
+		bw.WriteBit(bit)
+	}
+
+	bw.Flush(One)
+
+	err = bw.WriteByte(0xAA)
+	if err != nil {
+		t.Error("unable to WriteByte")
+	}
+
+	c := buf.Bytes()
+
+	if len(c) != 2 || c[1] != 0xAA || c[0] != 0x3f {
+		t.Error("bad return from 4 read bytes")
+	}
+
+	_, err = NewReader(strings.NewReader("")).ReadBit()
+	if err != io.EOF {
+		t.Error("ReadBit on empty string didn't return EOF")
+	}
+
+}
+
 func TestBitStream(t *testing.T) {
 
 	buf := bytes.NewBuffer(nil)
